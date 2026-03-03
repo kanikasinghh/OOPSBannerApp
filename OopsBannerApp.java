70ce372e70a0d7393e1aa248611d2026fca52af1
kanikasinghh <kanikaasinghh56@gmail.com>
2026-03-03 09:59:16 +0530

    added UC3

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -2,7 +2,7 @@
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
-}*/
+}
 
 public class OopsBannerApp {
     public static void main(String[] args) {
@@ -16,5 +16,20 @@ public class OopsBannerApp {
         System.out.println("      ***      ***     **        *****         ");
 
     }
+}*/
+
+public class OopsBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("", "      ***      ***      *****       ***** "));
+        System.out.println(String.join("", "     ** **    ** **    **   **    **      "));
+        System.out.println(String.join("", "    **   **  **   **   **   **   **       "));
+        System.out.println(String.join("", "    **   **  **   **   *****      ***     "));
+        System.out.println(String.join("", "    **   **  **   **   **            **   "));
+        System.out.println(String.join("", "     ** **    ** **    **             **  "));
+        System.out.println(String.join("", "      ***      ***     **        *****    "));
+
+    }
 }
+
 
