69484b2f61ce95fda10d0ebf8f2edda04dc93b7e
kanikasinghh <kanikaasinghh56@gmail.com>
2026-03-03 10:06:29 +0530

    added UC4

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -16,7 +16,7 @@ public class OopsBannerApp {
         System.out.println("      ***      ***     **        *****         ");
 
     }
-}*/
+}
 
 public class OopsBannerApp {
     public static void main(String[] args) {
@@ -29,6 +29,29 @@ public class OopsBannerApp {
         System.out.println(String.join("", "     ** **    ** **    **             **  "));
         System.out.println(String.join("", "      ***      ***     **        *****    "));
 
+    }
+}*/
+
+public class OopsBannerApp {
+
+    public static void main(String[] args) {
+
+    
+        String[] banner = new String[7];
+
+    
+        banner[0] = String.join("", "      ***      ***      *****       ***** ");
+        banner[1] = String.join("", "     ** **    ** **    **   **    **      ");
+        banner[2] = String.join("", "    **   **  **   **   **   **   **       ");
+        banner[3] = String.join("", "    **   **  **   **   *****      ***     ");
+        banner[4] = String.join("", "    **   **  **   **   **            **   ");
+        banner[5] = String.join("", "     ** **    ** **    **             **  ");
+        banner[6] = String.join("", "      ***      ***     **        *****    ");
+
+        
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
 
